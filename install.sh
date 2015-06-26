abc618995fdccb42fa1068c98a261b17c1f338ff
Roopak Parikh <rparikh@platform9.net>
2015-06-26 06:27:02 +0000

    An uber install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# A simple script to install few important pieces I use day to day
+
+PKG_MGR="yum"
+OS="Fedora"
+
+if [ -f /etc/debian_version ]; then
+	OS="Ubuntu"
+fi
+echo "OS detected is $OS"
+
+if [ "$OS" = "Ubuntu" ]; then
+	PKG_MGR="apt-get"
+fi
+
+$PKG_MGR -y install tmux
+$PKG_MGR -y install mosh
+$PKG_MGR -y install gdb
+$PKG_MGR -y install fish
+
+./vim-helper/setup-vim
+
